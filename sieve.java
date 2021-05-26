f06bcb2e09d60fda76bf0fed2b66190f093aabb1
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2021-05-26 17:23:07 +0530

    Update sieve.java

--- sieve.java
+++ sieve.java
@@ -3,13 +3,12 @@ import java.util.*;
 
 public class Template {
 	static boolean[] sieve(int N) {
-		boolean[] sieve = new boolean[N];
-		for(int i = 2; i < N; i++) sieve[i] = true;
+		boolean[] sieve = new boolean[N+1];
+		for(int i = 2; i <= N; i++) sieve[i] = true;
 
-		for(int i = 2; i < N; i++) {
+		for(int i = 2; i <= N; i++) {
 			if(sieve[i]) {
-				if(i*i < 0) continue;
-				for(int j = i*i; j < N; j += i) {
+				for(int j = 2*i; j <= N; j += i) {
 					sieve[j] = false; 
 				}
 			}

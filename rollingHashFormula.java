f8751760699af6815eaf0f4a7124f4574c383f81
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2021-06-13 20:10:08 +0530

    Update rollingHashFormula.java

--- rollingHashFormula.java
+++ rollingHashFormula.java
@@ -1,9 +1,9 @@
 static int hash(String s) {
-		int ans = 0;
-		int power = 1;
-		for(int i = 0; i < s.length(); i++) {
-			ans = (ans%mod + (((s.charAt(i)-'a')+1) * power) % mod)%mod;
-      power = (power%mod * 31%mod)%mod;
-		}
-		return ans;
+	int ans = 0;
+	int power = 1;
+	for(int i = 0; i < s.length(); i++) {
+		ans = (ans%mod + (((s.charAt(i)-'a')+1) * power) % mod)%mod;
+		power = (power%mod * 31%mod)%mod;
+	}
+	return ans;
 }

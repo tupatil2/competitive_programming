5c9fb5aa946c048a2cd3e31b94209086d586fd1e
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2021-06-14 16:41:42 +0530

    Update rabinKarp.java

--- rabinKarp.java
+++ rabinKarp.java
@@ -1,33 +1,35 @@
 static long rabinKarp(String s, String t) {
-		
-  // check how many times s occurs in t in O(s+t)
+	// check how many times s occurs in t
 
-  int S = s.length(), T = t.length();
+	s = s.toLowerCase();
+	t = t.toLowerCase();
 
-  long[] power = new long[n];
-  power[0] = 1;
-  int p = 31;
-  for(int i = 1; i < n; i++) {
-    power[i] = (power[i-1]%mod * p%mod)%mod;
-  }
+	int S = s.length(), T = t.length();
 
-  long[] tHash = new long[T+1];
+	long[] power = new long[T];
+	long p = 31;
+	power[0] = 1;
+	for(int i = 1; i < T; i++) {
+		power[i] = (power[i-1] * p) % mod;
+	}
 
-  for(int i = 0; i < T; i++) {
-    tHash[i+1] = (tHash[i] + (t.charAt(i)-'a' + 1) * power[i])%mod;
-  }
+	long[] tHash = new long[T+1];
+	for(int i = 0; i < T; i++) {
+		tHash[i+1] = (tHash[i] + (t.charAt(i)-'a'+1) * power[i]) % mod; 
+	}
 
-  long sHash = 0;
-  for(int i = 0; i < S; i++) {
-    sHash = (sHash + (s.charAt(i)-'a'+1) * power[i]) % mod;
-  }
+	long sHash = 0;
+	for(int i = 0; i < S; i++) {
+		sHash = (sHash + (s.charAt(i)-'a'+1) *  power[i]) % mod; 
+	}
 
-  long count = 0;
-  for(int i = 0; i < T - S + 1; i++) {
-    long curr = (tHash[i+S] - tHash[i] + mod)%mod;
-    if(curr == (sHash%mod * power[i]%mod)%mod) {
-      count++;
-    }
-  }
-  return count;
+	long count = 0l;
+	for(int i = 0; i < T; i++) {
+		if(i+S > T) break;
+		long curr = (tHash[i+S] - tHash[i] + mod) % mod;
+		if(curr == (sHash * power[i]) % mod) {
+			count++;
+		}
+	}
+	return count;
 }

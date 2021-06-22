9b5597e2b2d4cf227f4192f49821dd3c37585fee
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2021-06-22 16:51:53 +0530

    Update treeAlgo.java

--- treeAlgo.java
+++ treeAlgo.java
@@ -23,45 +23,46 @@ private static void subTreeSize(int curr, int[] size, List<List<Integer>> list, 
 }
 
 // lca using binary lifting
-static ArrayList<Integer> g[];
-static int memo[][], lev[], log;
-// memo -> dp, lev -> level, log -> log(n), g -> graph
+public static void main(String[] args){
+	int n = sc.nextInt();
+	int log = (int)Math.ceil(Math.log(n) / Math.log(2));
+	int[][] memo = new int[n + 1][log + 1];
+	dfs(0,0,list,log,level,memo); // for 0-indexed
+	// call lca from here. Also check if vertex is 0 index or 1 indexed and change it in dfs function accordingly.
+}
 
-static void dfs(int u, int p)
+static void dfs(int u, int p, List<List<Integer>> list, int log, int[] level, int[][] memo)
 {
     memo[u][0] = p;
     for (int i = 1; i <= log; i++)
-        memo[u][i] = memo[memo[u][i - 1]][i - 1];
-    for (int v : g[u]) {
-        if (v != p) {
-            lev[v] = lev[u] + 1;
-            dfs(v, u);
-        }
+	memo[u][i] = memo[memo[u][i - 1]][i - 1];
+    List<Integer> neighbours = list.get(u);
+    for (int v : neighbours) {
+	if (v != p) {
+	    level[v] = level[u] + 1;
+	    dfs(v, u, list, log, level, memo);
+	}
     }
 }
 
-static int lca(int u, int v)
+static int lca(int u, int v, List<List<Integer>> list, int[] level, int log, int[][] memo)
 {
-    if (lev[u] < lev[v]) {
-        int temp = u;
-        u = v;
-        v = temp;
+    if (level[u] < level[v]) {
+	int temp = u;
+	u = v;
+	v = temp;
     }
-
     for (int i = log; i >= 0; i--) {
-        if ((lev[u] - (int)Math.pow(2, i)) >= lev[v])
-            u = memo[u][i];
+	if ((level[u] - (int)Math.pow(2, i)) >= level[v])
+	    u = memo[u][i];
     }
-
     if (u == v)
-        return u;
-
+	return u;
     for (int i = log; i >= 0; i--) {
-        if (memo[u][i] != memo[v][i]) {
-            u = memo[u][i];
-            v = memo[v][i];
-        }
+	if (memo[u][i] != memo[v][i]) {
+	    u = memo[u][i];
+	    v = memo[v][i];
+	}
     }
-
     return memo[u][0];
 }

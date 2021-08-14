523239909af630c37fbe2e25f03b25b9e42b5ea0
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2021-08-14 12:39:16 +0530

    Update fibInLogN.java

--- fibInLogN.java
+++ fibInLogN.java
@@ -33,3 +33,42 @@
       }
       return ans[0][0];
  }
+
+// for long
+
+static long fibInLogN(long n){
+   n--;
+   long[][] base = {{1l,1l},{1l,0l}};
+   long[][] ans = {{1l,0l},{0l,1l}};
+   while(n != 0){
+       if(n%2 == 0){
+           // base = base * base and n = n/2
+           long temp[][] = new long[2][2];
+           temp[0][0] = add(mul(base[0][0],base[0][0]), mul(base[0][1],base[1][0]));
+           temp[0][1] = add(mul(base[0][0],base[0][1]), mul(base[0][1],base[1][1]));
+           temp[1][0] = add(mul(base[1][0],base[0][0]), mul(base[1][1],base[1][0]));
+           temp[1][1] = add(mul(base[1][0],base[0][1]), mul(base[1][1],base[1][1]));
+           base[0][0] = temp[0][0]; base[0][1] = temp[0][1]; base[1][0] = temp[1][0]; base[1][1] = temp[1][1];
+           n = n/2;
+       }
+       else {
+           // ans = ans * base and n = n-1
+           long temp[][] = new long[2][2];
+           temp[0][0] = add(mul(ans[0][0],base[0][0]), mul(ans[0][1],base[1][0]));
+           temp[0][1] = add(mul(ans[0][0],base[0][1]), mul(ans[0][1],base[1][1]));
+           temp[1][0] = add(mul(ans[1][0],base[0][0]), mul(ans[1][1],base[1][0]));
+           temp[1][1] = add(mul(ans[1][0],base[0][1]), mul(ans[1][1],base[1][1]));
+           ans[0][0] = temp[0][0]; ans[0][1] = temp[0][1]; ans[1][0] = temp[1][0]; ans[1][1] = temp[1][1];
+           n = n-1;
+       }
+   }
+   return ans[0][0];
+}
+
+public static long add(long a, long b){
+   return ((a+b)+mod)%mod;
+}
+
+public static long mul(long a, long b){
+   return ((a*b)+mod)%mod;
+}

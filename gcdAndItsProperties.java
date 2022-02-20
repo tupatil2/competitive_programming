184500758c99c886d84b0b3d17117e92796c6be7
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2022-02-20 20:07:20 +0530

    Update gcdAndItsProperties.java

--- gcdAndItsProperties.java
+++ gcdAndItsProperties.java
@@ -1,7 +1,7 @@
 static long gcd(long a, long b) {
-		if (b == 0)
-			return a;
-		return gcd(b, a % b);
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
 }
 
 /*
@@ -10,4 +10,5 @@ Properties of GCD
 2. gcd(a,b) = gcd(b,a%b)
 3. gcd(a,b) = gcd(b,abs(a-b)) = gcd(a,abs(a-b))
 4. gcd(a,b,c) = gcd(a,gcd(b,c))
+5. if (x * y) % k == 0 then (gcd(x,k) * gcd(y,k)) % k == 0
 */

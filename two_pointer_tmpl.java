9754bb7968fd558b80ad3a7d9f8629d838e23df0
tusharr-patil <patiltusharvikas@gmail.com>
2024-03-30 20:58:58 +0530

    2 ptr

---
+++ two_pointer_tmpl.java
@@ -0,0 +1,21 @@
+// generic templ for two pointers algo
+public int countAtKDistinct(int[] nums, int k) {
+    int n = nums.length;
+    int l = 0;
+    int r = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+    int count = 0;
+    while(r < n) {
+        map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+        while(map.size() > k) {
+            map.put(nums[l], map.get(nums[l]) - 1);
+            if(map.get(nums[l]) == 0) {
+                map.remove(nums[l]);
+            }
+            l++;
+        }
+        count += (r - l + 1);
+        r++;      
+    }
+    return count;
+}
